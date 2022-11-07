99a852ddc45c6942f8505d23b44ab40af3b853a6
Gorkem Ridvan Arik <76165528+sugrado@users.noreply.github.com>
2022-11-07 19:10:33 +0300

    Update multithreading.c

--- multithreading.c
+++ multithreading.c
@@ -5,27 +5,29 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-void main(int argc, char *argv[]){
-	if(argc < 3){
-		printf("[HW1] Calistirilacak programin output uzantisiyla beraber dosya adi ve kac sn calisacagi bilgisini parametre olarak girmelisiniz.");
-		return;
-	}
-	printf("[HW1] %s sn boyunca %s calisacak.\n",argv[2],argv[1]);
-	int convertedSecond = atoi(argv[2]);
-	pid_t pid = fork();
-	if(pid==0){
-		char x[]="./";
-        	strcat(x,argv[1]);
-		char *args[]={x,NULL};
-		if(execv(args[0],args)){
-			printf("[HW1] dosya bulunamadi!");
-			return;
-		}	
-	}else if (pid>0){
-		sleep(convertedSecond);
-		kill(pid,SIGKILL);
-		printf("[HW1]%d sn sona erdi %s kapatiliyor...\n",convertedSecond, argv[1]);
-	}
+void main(int argc, char * argv[]) {
+  if (argc < 3) {
+    printf("[Main] You must enter the first (output of the file to be run with .o extension) and second (how many seconds will it run) parameters.");
+    return;
+  }
 
-	return;
+  printf("[Main] %s will run for %s seconds.\n", argv[1], argv[2]);
+  int convertedSecond = atoi(argv[2]);
+  pid_t pid = fork();
+  if (pid == 0) {
+    char runCommand[] = "./";
+    strcat(runCommand, argv[1]);
+    char * args[] = {
+      runCommand,
+      NULL
+    };
+    if (execv(args[0], args)) {
+      printf("[Main] File not found!");
+      return;
+    }
+  } else if (pid > 0) {
+    sleep(convertedSecond);
+    kill(pid, SIGKILL);
+    printf("[Main] %d seconds is over. %s terminated.\n", convertedSecond, argv[1]);
+  }
 }
